b43afd42a1e9e0bcdd1f01737117cd7e31e381be
ibramheshmat <ibramheshmatisaac@gmail.com>
2022-05-19 22:43:44 +0200

    start project

---
+++ PhotoList_FlickrApp/PhotoList_FlickrApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoList_FlickrApp
+//
+//  Created by Ibram on 19/05/2022.
+//  Copyright © 2022 Ibram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoList_FlickrApp/PhotoList_FlickrAppTests/PhotoList_FlickrAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoList_FlickrAppTests.swift
+//  PhotoList_FlickrAppTests
+//
+//  Created by Ibram on 19/05/2022.
+//  Copyright © 2022 Ibram. All rights reserved.
+//
+
+import XCTest
+@testable import PhotoList_FlickrApp
+
+class PhotoList_FlickrAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
