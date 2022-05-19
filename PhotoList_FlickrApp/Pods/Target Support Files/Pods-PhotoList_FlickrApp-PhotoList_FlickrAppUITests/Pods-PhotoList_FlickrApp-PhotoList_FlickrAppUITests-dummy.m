b610dfb8296c38427620d3ca2e0ef4a5e80e923c
ibramheshmat <ibramheshmatisaac@gmail.com>
2022-05-19 22:53:50 +0200

    add pods

---
+++ PhotoList_FlickrApp/Pods/Target Support Files/Pods-PhotoList_FlickrApp-PhotoList_FlickrAppUITests/Pods-PhotoList_FlickrApp-PhotoList_FlickrAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoList_FlickrApp_PhotoList_FlickrAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoList_FlickrApp_PhotoList_FlickrAppUITests
+@end
---
+++ PhotoList_FlickrApp/Pods/Target Support Files/Pods-PhotoList_FlickrAppTests/Pods-PhotoList_FlickrAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoList_FlickrAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoList_FlickrAppTests
+@end
